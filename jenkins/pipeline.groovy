736ca48321eec6a4e340a7453a14a40d8cb1b1aa
Neelesh <ngurjar@neeleshgurjar.co.in>
2020-01-08 15:58:58 +0530

    Added POST declarative action

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -24,14 +24,14 @@ pipeline {
          steps {
             // Provision Instance
             script {
-                def ami_id = sh (script: "grep us-east-1 /tmp/${env.BUILD_NUMBER}_packer_build.log|awk -F': ' '{print \$2}'", returnStdout: true).trim()
+                def ami_id = sh (script: "grep us-east-1 /tmp/72_packer_build.log|awk -F': ' '{print \$2}'", returnStdout: true).trim()
                 echo "AMI ID is ${ami_id}"
                 sh("cd tf-deploy && terraform init")
                 sh("cd tf-deploy && terraform apply -auto-approve -var 'ami_id=${ami_id}' -var 'instance_type=${instance_type}' -var 'vpc_id=${vpc_id}' -var 'subnet_id=${subnet_id}' -var 'ec2_key_name=${ec2_key_name}' -var 'scanner_ip_ranges=${scanner_ip_ranges}' >/tmp/${env.BUILD_NUMBER}_tf_deploy.log")
             }
          }
       }
-      stage('VM Scan') {
+      stage('Scan VM') {
          steps {
             // Scan Instance
             script {
@@ -39,22 +39,22 @@ pipeline {
                 def instance_priv_ip = sh (script: "grep 'instance_private_ip =' /tmp/${env.BUILD_NUMBER}_tf_deploy.log|awk -F'= ' '{print \$2}'", returnStdout: true).trim()
                 echo "Public IP of instance is ${instance_pub_ip}"
                 echo "Private IP of instance is ${instance_priv_ip}"
-                withCredentials([usernamePassword(credentialsId: '${qualys_creds_id}', passwordVariable: 'pass', usernameVariable: 'user')]) {
+                withCredentials([usernamePassword(credentialsId: "${qualys_creds_id}", passwordVariable: 'pass', usernameVariable: 'user')]) {
                   sh("curl -H 'X-Requested-With: Curl Sample' -u '$user:$pass' -X 'POST' -d 'action=add&enable_vm=1&ips=${instance_pub_ip}' 'https://${qualys_url}/api/2.0/fo/asset/ip/'")
                 }
-                qualysVulnerabilityAnalyzer apiServer: '${qualys_url}', bySev: 5, credsId: 'Qualys - Free Trial', failBySev: true, hostIp: "${instance_pub_ip}", optionProfile: 'new-ec2-access-profile', pollingInterval: '2', scanName: '[job_name]_jenkins_build_[build_number]', scannerName: 'External', useHost: true, vulnsTimeout: '60*2'
+                  qualysVulnerabilityAnalyzer apiServer: "https://${qualys_url}", bySev: 1, credsId: 'Qualys - Free Trial', evaluatePotentialVulns: true, failByPci: true, failBySev: true, hostIp: "${instance_pub_ip}", optionProfile: "${qualys_option_profile_name}", pollingInterval: '2', scanName: '[job_name]_jenkins_build_[build_number]', scannerName: 'External', useHost: true, vulnsTimeout: '60*2'
             }
          }
       }
-      stage('CleanUp') {
-         steps {
-            // Cleanup Instance
-            script {
-                def ami_id = sh (script: "grep us-east-1 /tmp/36_packer_build.log|awk -F': ' '{print \$2}'", returnStdout: true).trim()
-                echo "AMI ID is ${ami_id}"
-                sh("cd tf-deploy && terraform destroy -auto-approve -var 'ami_id=${ami_id}' -var 'instance_type=${instance_type}' -var 'vpc_id=${vpc_id}' -var 'subnet_id=${subnet_id}' -var 'ec2_key_name=${ec2_key_name}' -var 'scanner_ip_ranges=${scanner_ip_ranges}'")
-            }
-         }
+  }
+   post {
+      always {
+         // Cleanup Instance
+        script {
+            def ami_id = sh (script: "grep us-east-1 /tmp/${env.BUILD_NUMBER}_packer_build.log|awk -F': ' '{print \$2}'", returnStdout: true).trim()
+            echo "AMI ID is ${ami_id}"
+            sh("cd tf-deploy && terraform destroy -auto-approve -var 'ami_id=${ami_id}' -var 'instance_type=${instance_type}' -var 'vpc_id=${vpc_id}' -var 'subnet_id=${subnet_id}' -var 'ec2_key_name=${ec2_key_name}' -var 'scanner_ip_ranges=${scanner_ip_ranges}'")
+        }
       }
    }
 }
